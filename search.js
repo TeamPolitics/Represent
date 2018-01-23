ea16c70dfb70ae2dc5bc251b4f8a464de1274a3a
sarleone <sarah.m.leone@gmail.com>
2018-01-22 21:32:48 -0600

    added geoAPI

--- search.js
+++ search.js
@@ -1,10 +1,37 @@
+var addressInputG = "3040d"
+//variable to store API from google civics
+var APIKeyG = "AIzaSyCuDsqDKUFb1QUNBy7-KughoBsoU7RkYRo";
+//queryUrl using address
+var queryURLG = "https://maps.googleapis.com/maps/api/geocode/json?key=" + APIKey + "&address=" + addressInputG;
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	//after data returns from request
+	.then(function(response) {
+			console.log(queryURL);
+			console.log(response);
+		//if response.status === zero_results push to div
+		if (response.status === "ZERO_RESULTS") {
+			$("#geo-error").text("Please reenter your zipcode")
+		//return formatted address
+		} else {
+			var location = 
+		}
+	}
+
+
+
+//variable for whatever the user inputs as address
+var addressInput = "60202";
+//variable to store API from google civics
+var APIKeyP = "AIzaSyB0nuZo-jOlCHEFS6UB15CYoc0koH2nm8o";
+//queryUrl using address
+var queryURLP = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + APIKey + "&address=" + addressInput;
+
 //Function to display info from Google Civic API
 function displayPoliInfo() {
-	//variable for whatever the user inputs as address
-	var addressInput = $(this).attr("data-address");
-	//queryUrl using address
-	var queryURL = "https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyB0nuZo-jOlCHEFS6UB15CYoc0koH2nm8o&address=" + addressInput;
-
 	//Performing an AJAX request with queryURL
 	$.ajax({
 		url: queryURL,
@@ -22,7 +49,7 @@ function displayPoliInfo() {
 		for (var i = 0; i < results.length; i++) {
 			
 			//creating and storing a div tag
-			var poliDiv = $("div class= 'item'>");
+			var poliDiv = $("<div>");
 
 			//creating a variable tag to hold politician name
 			var poliName = results[i].officials.name;
@@ -34,5 +61,11 @@ function displayPoliInfo() {
 
 
 		}
+
+	}, function(error){
+		console.log(JSON.parse(error.responseText))
+		;
 	});
 }
+
+displayPoliInfo();
