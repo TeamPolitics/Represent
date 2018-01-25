eafc3e763a8f66a489c866ff62de9127643bb0b4
sarleone <sarah.m.leone@gmail.com>
2018-01-24 20:51:23 -0600

    div displays Rep Name

--- search.js
+++ search.js
@@ -1,16 +1,21 @@
-var addressInputG = "3040d"
+var addressInputG = "60202"
+
+	if (isNaN(addressInputG)
+		|| addressInputG.length != 5) {
+		alert("Please enter a valid zipcode!")
+	}
 //variable to store API from google civics
 var APIKeyG = "AIzaSyCuDsqDKUFb1QUNBy7-KughoBsoU7RkYRo";
 //queryUrl using address
-var queryURLG = "https://maps.googleapis.com/maps/api/geocode/json?key=" + APIKey + "&address=" + addressInputG;
+var queryURLG = "https://maps.googleapis.com/maps/api/geocode/json?key=" + APIKeyG + "&address=" + addressInputG;
 
 	$.ajax({
-		url: queryURL,
+		url: queryURLG,
 		method: "GET"
 	})
 	//after data returns from request
 	.then(function(response) {
-			console.log(queryURL);
+			console.log(queryURLG);
 			console.log(response);
 		//if response.status === zero_results push to div
 		if (response.status === "ZERO_RESULTS") {
@@ -20,8 +25,9 @@ var queryURLG = "https://maps.googleapis.com/maps/api/geocode/json?key=" + APIKe
 			var location = response.results[0].geometry;
 			console.log(location);
 			alert("it's working")
+
 		}
-	}
+	 });
 
 
 
@@ -30,44 +36,32 @@ var addressInput = "60202";
 //variable to store API from google civics
 var APIKeyP = "AIzaSyB0nuZo-jOlCHEFS6UB15CYoc0koH2nm8o";
 //queryUrl using address
-var queryURLP = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + APIKey + "&address=" + addressInput;
+var queryURLP = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + APIKeyP + "&address=" + addressInput;
 
 //Function to display info from Google Civic API
 function displayPoliInfo() {
 	//Performing an AJAX request with queryURL
 	$.ajax({
-		url: queryURL,
+		url: queryURLP,
 		method: "GET"
 	})
 		//after data returns from request
 		.then(function(response) {
-			console.log(queryURL);
+			console.log(queryURLP);
 			console.log(response);
 
-		//storing the data from AJAX request in the results variable
-		var results = response.data;
-
-		//Looking through the result of each item
-		for (var i = 0; i < results.length; i++) {
-			
-			//creating and storing a div tag
-			var poliDiv = $("<div>");
-
 			//creating a variable tag to hold politician name
-			var poliName = results[i].officials.name;
+			var poliName = response.officials[3].name;
 
 			//creating a paragraph tag with result of politician nam
 			var p = $("<p>").text("Your representative's name: " + poliName);
 
-		$("#poli-info-here").prepend(poliDiv);
+		$("#poli-info-here").prepend(p);
 
 
-		}
+		})
 
-	}, function(error){
-		console.log(JSON.parse(error.responseText))
-		;
-	});
+	
 }
 
 displayPoliInfo();
